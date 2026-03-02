8d3be8990bd37a508f164d279b89276deffa9609
ranggays <ranggayoviesaputra@gmail.com>
2026-03-02 07:44:07 +0700

    add: config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'gradient-x': 'gradient-x 15s ease infinite',
+        'blob': 'blob 7s infinite',
+      },
+    },
+  },
+  plugins: [],
+}
